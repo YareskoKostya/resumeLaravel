022e27f8426dd5443aa568b60329f6492a3a4655
YareskoKostya <zhtcmrj@gmail.com>
2018-10-25 19:44:08 +0300

    ch table resumes

--- database/migrations/2018_10_25_161607_create_resumes_table.php
+++ database/migrations/2018_10_25_161607_create_resumes_table.php
@@ -15,6 +15,28 @@ class CreateResumesTable extends Migration
     {
         Schema::create('resumes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->date('birthdate');
+            $table->string('country');
+            $table->string('tel');
+            $table->string('email');
+            $table->string('address');
+            for ($i = 1; $i <= 3; $i++){
+                $table->date("datebeginstudy$i");
+                $table->date("dateendstudy$i");
+                $table->string("studyname$i");
+                $table->string("proffessionstudy$i");
+                $table->string("doctor$i");
+            }
+            for ($j = 1; $j <= 3; $j++){
+                $table->date("datebeginwork$j");
+                $table->date("dateendwork$j");
+                $table->string("workname$j");
+                $table->string("proffessionwork$j");
+            }
+            $table->string('interests');
+            $table->binary('photo');
             $table->timestamps();
         });
     }
