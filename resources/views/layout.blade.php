109a9d09284b81633e670e0007d17aab35765de0
YareskoKostya <zhtcmrj@gmail.com>
2018-10-28 17:23:59 +0200

    cr main css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+
     <title>Resume</title>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
