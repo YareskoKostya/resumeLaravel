a1cdd0e0589cff7fdb456530ac5c0d6767425010
YareskoKostya <zhtcmrj@gmail.com>
2018-10-31 20:36:32 +0200

    add fillable

--- app/Resume.php
+++ app/Resume.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resume extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'surname',
+        'birthdate',
+        'country',
+        'tel',
+        'email',
+        'address',
+        'datebeginstudy0',
+        'dateendstudy0',
+        'studyname0',
+        'professionstudy0',
+        'doctor0',
+        'datebeginwork',
+        'dateendwork',
+        'workname',
+        'professionwork',
+        'interests',
+        'photo'
+    ];
 }
